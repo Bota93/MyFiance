6d4a3f64eee1a148677cadb1fb838417c933d97d
Bota93 <a.alcaraz.rodri@gmail.com>
2025-09-09 09:00:29 +0200

    Cambio versiones

--- myfiance-frontend/src/components/layout/Header.tsx
+++ myfiance-frontend/src/components/layout/Header.tsx
@@ -22,7 +22,7 @@ export default function Header({ title, onAddTransaction }: HeaderProps) {
      */
     const handleLogout = () => {
         localStorage.removeItem('authToken');
-        router.push('/login');
+        router.push('/');
     };
 
     return (
